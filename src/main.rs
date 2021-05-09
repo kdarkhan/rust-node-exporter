29df649ebf730176f66900f1fba41534e8655329
Darkhan Kubigenov <darkhanu@gmail.com>
2021-05-10 00:39:42 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+mod helper;
+
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+
+        println!("Connection established!");
+    }
+    println!("Hello, world!");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let contents = helper::get_response_string();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
