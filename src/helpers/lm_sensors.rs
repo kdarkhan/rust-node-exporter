4c996d1913d5047781cf107eefe835b25285c633
Darkhan Kubigenov <darkhanu@gmail.com>
2021-05-19 01:28:21 +0100

    Switch lm_sensors json parser to serde

--- src/helpers/lm_sensors.rs
+++ src/helpers/lm_sensors.rs
@@ -1,68 +1,111 @@
-use serde_json::Value;
 use std::process::Command;
 use std::str;
 
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Sensors {
+    #[serde(rename = "k10temp-pci-00c3")]
+    k10temp: K10Temp,
+    #[serde(rename = "asuswmisensors-isa-0000")]
+    asus_wmi_sensors: AsusWmiSensors,
+}
+
+#[derive(Deserialize)]
+struct K10Temp {
+    #[serde(rename = "Tctl")]
+    tctl: F64Wrapper,
+    #[serde(rename = "Tdie")]
+    tdie: F64Wrapper,
+}
+
+#[derive(Deserialize)]
+struct AsusWmiSensors {
+    #[serde(rename = "CPU Core Voltage")]
+    core_voltage: F64Wrapper,
+    #[serde(rename = "+12V Voltage")]
+    plus_12_voltage: F64Wrapper,
+    #[serde(rename = "+5V Voltage")]
+    plus_5_voltage: F64Wrapper,
+    #[serde(rename = "3VSB Voltage")]
+    three_vsb_voltage: F64Wrapper,
+    #[serde(rename = "CPU Fan")]
+    cpu_fan: F64Wrapper,
+    #[serde(rename = "Chassis Fan 2")]
+    chassis_fan_2: F64Wrapper,
+    #[serde(rename = "CPU Temperature")]
+    cpu_temperature: F64Wrapper,
+    #[serde(rename = "Motherboard Temperature")]
+    mb_temperature: F64Wrapper,
+    #[serde(rename = "Chipset Temperature")]
+    chipset_temperature: F64Wrapper,
+}
+
+#[derive(Deserialize)]
+struct F64Wrapper {
+    #[serde(
+        alias = "temp1_input",
+        alias = "temp2_input",
+        alias = "temp3_input",
+        alias = "temp4_input",
+        alias = "in0_input",
+        alias = "in1_input",
+        alias = "in2_input",
+        alias = "in3_input",
+        alias = "fan1_input",
+        alias = "fan2_input",
+        alias = "fan3_input",
+        alias = "fan4_input"
+    )]
+    value: f64,
+}
+
 pub fn get_lm_sensor_metrics() -> String {
     match Command::new("sensors").arg("-j").output() {
-        Ok(output) => match str::from_utf8(&output.stdout) {
-            Ok(out) => {
-                let v: Value = serde_json::from_str(out).expect("sensors -j did not return json");
-                let tctl = v["k10temp-pci-00c3"]["Tctl"]["temp1_input"]
-                    .as_f64()
-                    .unwrap_or(0.0);
-                let tdie = v["k10temp-pci-00c3"]["Tdie"]["temp2_input"]
-                    .as_f64()
-                    .unwrap_or(0.0);
+        Ok(output) => {
+            let sens: Sensors = serde_json::from_slice(&output.stdout).unwrap();
 
-                let core_voltage = v["asuswmisensors-isa-0000"]["CPU Core Voltage"]["in0_input"]
-                    .as_f64()
-                    .unwrap_or(0.0);
-                let plus_5_voltage = v["asuswmisensors-isa-0000"]["+5V Voltage"]["in2_input"]
-                    .as_f64()
-                    .unwrap_or(0.0);
-                let plus_12_voltage = v["asuswmisensors-isa-0000"]["+12V Voltage"]["in1_input"]
-                    .as_f64()
-                    .unwrap_or(0.0);
-                let three_vsb_voltage = v["asuswmisensors-isa-0000"]["3VSB Voltage"]["in3_input"]
-                    .as_f64()
-                    .unwrap_or(0.0);
-                let cpu_temp = v["asuswmisensors-isa-0000"]["CPU Temperature"]["temp1_input"]
-                    .as_f64()
-                    .unwrap_or(0.0);
-                let mb_temp = v["asuswmisensors-isa-0000"]["Motherboard Temperature"]
-                    ["temp2_input"]
-                    .as_f64()
-                    .unwrap_or(0.0);
-                let chipset_temp = v["asuswmisensors-isa-0000"]["Chipset Temperature"]
-                    ["temp3_input"]
-                    .as_f64()
-                    .unwrap_or(0.0);
-                let cpu_fan = v["asuswmisensors-isa-0000"]["CPU Fan"]["fan1_input"]
-                    .as_f64()
-                    .unwrap_or(0.0);
-                let chassis_fan = v["asuswmisensors-isa-0000"]["Chassis Fan 2"]["fan3_input"]
-                    .as_f64()
-                    .unwrap_or(0.0);
-
-                let mut result = String::new();
-                result.push_str(&format!("\nlm_sensors_tctl {}", tctl));
-                result.push_str(&format!("\nlm_sensors_tdie {}", tdie));
-                result.push_str(&format!("\nlm_sensors_cpu_temp {}", cpu_temp));
-                result.push_str(&format!("\nlm_sensors_mb_temp {}", mb_temp));
-                result.push_str(&format!("\nlm_sensors_chipset_temp {}", chipset_temp));
-                result.push_str(&format!("\nlm_sensors_cpu_fan {}", cpu_fan));
-                result.push_str(&format!("\nlm_sensors_chassis_fan {}", chassis_fan));
-                result.push_str(&format!("\nlm_sensors_core_voltage {}", core_voltage));
-                result.push_str(&format!("\nlm_sensors_plus_5_voltage {}", plus_5_voltage));
-                result.push_str(&format!("\nlm_sensors_plus_12_voltage {}", plus_12_voltage));
-                result.push_str(&format!("\nlm_sensors_3vsb_voltage {}", three_vsb_voltage));
-                return result;
-            }
-            Err(e) => {
-                eprintln!("error parsing lm_sensors stdout {}", e);
-                panic!()
-            }
-        },
+            let mut result = String::new();
+            result.push_str(&format!("\nlm_sensors_tctl {}", sens.k10temp.tctl.value));
+            result.push_str(&format!("\nlm_sensors_tdie {}", sens.k10temp.tdie.value));
+            result.push_str(&format!(
+                "\nlm_sensors_cpu_temp {}",
+                sens.asus_wmi_sensors.cpu_temperature.value
+            ));
+            result.push_str(&format!(
+                "\nlm_sensors_mb_temp {}",
+                sens.asus_wmi_sensors.mb_temperature.value
+            ));
+            result.push_str(&format!(
+                "\nlm_sensors_chipset_temp {}",
+                sens.asus_wmi_sensors.chipset_temperature.value
+            ));
+            result.push_str(&format!(
+                "\nlm_sensors_cpu_fan {}",
+                sens.asus_wmi_sensors.cpu_fan.value
+            ));
+            result.push_str(&format!(
+                "\nlm_sensors_chassis_fan {}",
+                sens.asus_wmi_sensors.chassis_fan_2.value
+            ));
+            result.push_str(&format!(
+                "\nlm_sensors_core_voltage {}",
+                sens.asus_wmi_sensors.core_voltage.value
+            ));
+            result.push_str(&format!(
+                "\nlm_sensors_plus_5_voltage {}",
+                sens.asus_wmi_sensors.plus_5_voltage.value
+            ));
+            result.push_str(&format!(
+                "\nlm_sensors_plus_12_voltage {}",
+                sens.asus_wmi_sensors.plus_12_voltage.value
+            ));
+            result.push_str(&format!(
+                "\nlm_sensors_3vsb_voltage {}",
+                sens.asus_wmi_sensors.three_vsb_voltage.value
+            ));
+            return result;
+        }
         Err(e) => {
             eprintln!("error running lm_sensors {}", e);
             panic!()
