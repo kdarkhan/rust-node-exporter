6ee11a2b8dd7eb33e574c592821e405ce0ac8440
Darkhan Kubigenov <darkhanu@gmail.com>
2022-01-08 23:22:31 +0000

    Update clap to latest version

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,7 @@ extern crate quick_xml;
 extern crate serde;
 mod helpers;
 
-#[macro_use]
-extern crate clap;
-
-use clap::App;
-use std::collections::HashSet;
+use clap::{ArgEnum, Parser};
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -16,36 +12,47 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::Instant;
 
-const EXPORTER_HDDTEMP: &str = "hddtemp";
-const EXPORTER_LM_SENSORS: &str = "lm_sensors";
-const EXPORTER_PROC_MEMINFO: &str = "proc_meminfo";
-const EXPORTER_PROC_NETDEV: &str = "proc_netdev";
-const EXPORTER_PROC_STAT: &str = "proc_stat";
-const EXPORTER_NVIDIA: &str = "nvidia";
-const EXPORTER_NZXT_AIO: &str = "nzxt_aio";
+#[derive(Parser)]
+#[clap(author, version, about)]
+#[clap(about = "Prometheus exporter for my desktop metrics")]
+struct Cli {
+    /// Port where the HTTP server is listening
+    #[clap(default_value_t = 7878, short = 'p')]
+    port: u32,
+
+    /// List of enabled exporters (all are enabled if none provided)
+    #[clap(arg_enum, short = 'x')]
+    exporters: Vec<Exporter>,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
+#[clap(rename_all = "snake_case")]
+enum Exporter {
+    Hddtemp,
+    LmSensors,
+    ProcMeminfo,
+    ProcNetdev,
+    ProcStat,
+    Nvidia,
+    NzxtAio,
+}
 
 fn main() {
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+    let cli = Cli::parse();
 
-    let port: i32 = matches.value_of("port").unwrap().parse().unwrap();
-
-    let exporters: HashSet<&str> = if matches.is_present("exporters") {
-        matches.values_of("exporters").unwrap().collect()
-    } else {
-        [
-            EXPORTER_LM_SENSORS,
-            EXPORTER_HDDTEMP,
-            EXPORTER_PROC_MEMINFO,
-            EXPORTER_PROC_NETDEV,
-            EXPORTER_PROC_STAT,
-            EXPORTER_NZXT_AIO,
-            EXPORTER_NVIDIA,
+    let port: u32 = cli.port;
+    let mut exporters: Vec<Exporter> = cli.exporters;
+    if exporters.is_empty() {
+        exporters = vec![
+            Exporter::Hddtemp,
+            Exporter::LmSensors,
+            Exporter::ProcMeminfo,
+            Exporter::ProcNetdev,
+            Exporter::ProcStat,
+            Exporter::Nvidia,
+            Exporter::NzxtAio,
         ]
-        .iter()
-        .cloned()
-        .collect()
-    };
+    }
 
     let mut aio_metrics = helpers::nzxt_aio::get_aio_metrics();
     let mut lm_sensors = helpers::lm_sensors::get_lm_sensors();
@@ -68,10 +75,10 @@ fn main() {
     })
     .expect("Error setting Ctrl-C handler");
 
-    if exporters.contains(EXPORTER_NZXT_AIO) {
+    if exporters.contains(&Exporter::NzxtAio) {
         aio_metrics.init();
     }
-    if exporters.contains(EXPORTER_LM_SENSORS) {
+    if exporters.contains(&Exporter::LmSensors) {
         lm_sensors.init();
     }
 
@@ -81,26 +88,26 @@ fn main() {
         stream.read(&mut buffer).unwrap();
 
         let mut result = String::new();
-        if exporters.contains(EXPORTER_NZXT_AIO) {
+        if exporters.contains(&Exporter::NzxtAio) {
             result.push_str(&aio_metrics.get_aio_metrics());
         }
-        if exporters.contains(EXPORTER_LM_SENSORS) {
+        if exporters.contains(&Exporter::LmSensors) {
             // result.push_str(&helpers::lm_sensors::get_lm_sensor_metrics());
             result.push_str(&lm_sensors.get_lm_sensor_metrics());
         }
-        if exporters.contains(EXPORTER_HDDTEMP) {
+        if exporters.contains(&Exporter::Hddtemp) {
             result.push_str(&helpers::hddtemp::get_hddtemp_metrics());
         }
-        if exporters.contains(EXPORTER_NVIDIA) {
+        if exporters.contains(&Exporter::Nvidia) {
             result.push_str(&helpers::nvidia::get_nvidia_metrics());
         }
-        if exporters.contains(EXPORTER_PROC_STAT) {
+        if exporters.contains(&Exporter::ProcStat) {
             result.push_str(&helpers::proc_stat::get_proc_stat());
         }
-        if exporters.contains(EXPORTER_PROC_NETDEV) {
+        if exporters.contains(&Exporter::ProcNetdev) {
             result.push_str(&helpers::proc_netdev::get_proc_netdev());
         }
-        if exporters.contains(EXPORTER_PROC_MEMINFO) {
+        if exporters.contains(&Exporter::ProcMeminfo) {
             result.push_str(&helpers::proc_meminfo::get_proc_memifo());
         }
 
