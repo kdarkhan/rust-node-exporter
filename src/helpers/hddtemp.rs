993714d4c22d81fc17b5ed756bfb9a67ff0b3e1d
Darkhan Kubigenov <darkhanu@gmail.com>
2022-06-06 15:41:34 +0100

    Parse new HDDs using hddtemp

--- src/helpers/hddtemp.rs
+++ src/helpers/hddtemp.rs
@@ -9,7 +9,7 @@ pub fn get_hddtemp_metrics() -> String {
 
     lazy_static! {
         static ref HDDTEMP_PATTERN: Regex = Regex::new(
-            r"\|CT1000MX500SSD4\|([0-9.]+)\|C\|.+\|Samsung SSD 860 EVO M.2\|([0-9.]+)\|C\|.*$"
+            r"\|CT1000MX500SSD4\|([0-9.]+)\|C\|.+\|Samsung SSD 860 EVO M.2\|([0-9.]+)\|C\|.+\|WDC WD20EFZX-68AWUN0\|([0-9.]+)\|C\|.+\|WDC WD20EFZX-68AWUN0\|([0-9.]+)\|C\|.*$"
         )
         .unwrap();
     }
@@ -22,9 +22,11 @@ pub fn get_hddtemp_metrics() -> String {
 
     if let Some(m) = HDDTEMP_PATTERN.captures(&res) {
         format!(
-            "hddtemp_crucial_mx500_temp {}\nhddtemp_samsung_860_evo_temp {}\n",
+            "hddtemp_crucial_mx500_temp {}\nhddtemp_samsung_860_evo_temp {}\nhddtemp_wd_red_plus_1_temp {}\nhddtemp_wd_red_plus_2_temp {}\n",
             m[1].to_string(),
-            m[2].to_string()
+            m[2].to_string(),
+            m[3].to_string(),
+            m[4].to_string(),
         )
     } else {
         panic!("Could not parse hddtemp output")
